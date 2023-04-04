7bd2224c8ad3b5fa650db624e28c57a7df262e4d
Alfred Kabba Swaray <90448350+AlfredKSwaray@users.noreply.github.com>
2023-04-04 12:25:48 +0200

    Update sign_up_screen.dart

--- lib/screens/sign_up/sign_up_screen.dart
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
-import 'components/body.dart';
+import 'sign_up_screen.dart';
 
-class SignUpScreen extends StatelessWidget {
-static String routeName = "/sign_up";
-
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text("Sign Up"),
-),
-body: Body(),
-);
+void main() {
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        SignUpScreen.routeName: (context) => SignUpScreen(),
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Sign Up'),
+          onPressed: () {
+            Navigator.pushNamed(context, SignUpScreen.routeName);
+          },
+        ),
+      ),
+    );
+  }
 }
