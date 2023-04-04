ccd836250f0acfd5b4ad5f419f2fce9c473213f3
Abraham Diress <a.diress@alustudent.com>
2023-04-04 18:03:37 +0200

    Revert "Update sign_up_screen.dart"
    This reverts commit cd83eda48cc489421c6e6cdf4448d5240214cd6b.

--- lib/screens/sign_up/sign_up_screen.dart
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -1,8 +1,62 @@
 import 'package:flutter/material.dart';
-import 'components/body.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   static String routeName = "/sign_up";
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  String _errorText = '';
+
+  void _createAccount() async {
+    String username = _usernameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    String confirmPassword = _confirmPasswordController.text;
+
+    // Validate input
+    if (username.isEmpty || email.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
+      setState(() {
+        _errorText = 'Please fill in all fields';
+      });
+      return;
+    }
+
+    if (password != confirmPassword) {
+      setState(() {
+        _errorText = 'Passwords do not match';
+      });
+      return;
+    }
+
+    // Make API call to register user
+    String url = 'https://your-api-endpoint.com/register';
+    var response = await http.post(Uri.parse(url), body: {
+      'username': username,
+      'email': email,
+      'password': password
+    });
+
+    if (response.statusCode == 200) {
+      // User was created successfully, navigate to login screen
+      Navigator.pushNamed(context, '/login');
+    } else {
+      // An error occurred, display error message to user
+      Map<String, dynamic> data = json.decode(response.body);
+      setState(() {
+        _errorText = data['error'];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +64,10 @@ class SignUpScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Sign Up"),
       ),
-      body: Body(),
-    );
-  }
-}
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+               
