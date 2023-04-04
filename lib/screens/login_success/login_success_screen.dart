1e7c5b88ca75e44a2606126bafa665d4575bea46
Abraham Diress <a.diress@alustudent.com>
2023-04-04 22:19:09 +0200

    auth bug fixed

--- lib/screens/login_success/login_success_screen.dart
+++ lib/screens/login_success/login_success_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-
-import 'components/body.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mini_project_mob_dev/screens/sign_in/sign_in_screen.dart';
 
 class LoginSuccessScreen extends StatelessWidget {
   static String routeName = "/login_success";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +12,35 @@ class LoginSuccessScreen extends StatelessWidget {
         leading: SizedBox(),
         title: Text("Login Success"),
       ),
-      body: Body(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Welcome,",
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 16),
+            Text(
+              "The main pages are under construction and will be in service very soon! (But still firebase works ;) )",
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 32),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange,
+                onPrimary: Colors.white,
+              ),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+                Navigator.pushNamed(context, SignInScreen.routeName);
+              },
+              child: Text("Sign Out"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
